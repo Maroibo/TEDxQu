ed4058247ef954af906b8549952a9812f8ddfce4
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-08-17 12:19:06 +0300

    add SponsorsPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainTheme from "./components/MainTheme.jsx";
 import Layout from "./pages/Layout.jsx";
 import About from "./pages/About.jsx";
 import JoinUsPage from "./pages/JoinUsPage.jsx";
+import SponsorsPage from "./pages/SponsorsPage.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="joinus" element={<JoinUsPage />} />
+            <Route path="sponsors" element={<SponsorsPage />} />
+            <Route path="tedxqu" element={<JoinUsPage />} />
             <Route path="MainTheme" element={<MainTheme />} />
           </Route>
         </Routes>
---
+++ src/pages/SponsorsPage.jsx
@@ -0,0 +1,13 @@
+import MainTheme from "../components/MainTheme";
+import Sponsors from "../components/Sponsers";
+
+const SponsorsPage = () => {
+  return (
+    <>
+      <MainTheme />
+      <Sponsors />
+    </>
+  );
+};
+
+export default SponsorsPage;
