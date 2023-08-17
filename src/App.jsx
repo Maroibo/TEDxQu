197d456f36b2e415b4a6a5050cd6531c4066fef2
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-08-17 11:12:38 +0300

    refactor: use react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-
-import NavMobile from "./components/NavMobile.jsx"
-import FooterMobile from "./components/FooterMobile.jsx"
-import Nav from "./components/Nav.jsx"
-import FooterDesktop from "./components/FooterDesktop.jsx";
 import WhatisTEDx from "./components/WhatisTEDx.jsx";
-import { useState, useEffect } from "react";
+import MainTheme from "./components/MainTheme.jsx";
+import Layout from "./pages/Layout.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  const handleWindowSizeChange = () => {
-    setScreenWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleWindowSizeChange);
-
-    return () => {
-      window.removeEventListener('resize', handleWindowSizeChange);
-    };
-  }, []);
-
-  const isMobile = screenWidth <= 768;
   return (
     <>
-      {isMobile ? <NavMobile /> : <Nav />}
-      <WhatisTEDx />
-      {isMobile ? <FooterMobile /> : <FooterDesktop />} 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<WhatisTEDx />} />
+            <Route path="MainTheme" element={<MainTheme />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
