5e7cbbb06af8599d8bd57b6e496420a79684782b
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-08-17 11:17:42 +0300

    add Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import WhatisTEDx from "./components/WhatisTEDx.jsx";
+import Home from "./pages/Home.jsx";
 import MainTheme from "./components/MainTheme.jsx";
 import Layout from "./pages/Layout.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<WhatisTEDx />} />
+            <Route index element={<Home />} />
             <Route path="MainTheme" element={<MainTheme />} />
           </Route>
         </Routes>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import QuEvent from "../components/QuEvent";
+import WhatisTEDx from "../components/WhatisTEDx";
+
+const Home = () => {
+  return (
+    <>
+      <QuEvent />
+      <WhatisTEDx />
+    </>
+  );
+};
+
+export default Home;
