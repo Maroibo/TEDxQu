f784d084f6196487af29d4c5249c53cb24572011
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-08-17 12:30:42 +0300

    add TEDxQU.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Home from "./pages/Home.jsx";
-import MainTheme from "./components/MainTheme.jsx";
 import Layout from "./pages/Layout.jsx";
 import About from "./pages/About.jsx";
 import JoinUsPage from "./pages/JoinUsPage.jsx";
 import SponsorsPage from "./pages/SponsorsPage.jsx";
+import TEDxQU from "./pages/TEDxQU.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -15,8 +15,7 @@ function App() {
             <Route path="about" element={<About />} />
             <Route path="joinus" element={<JoinUsPage />} />
             <Route path="sponsors" element={<SponsorsPage />} />
-            <Route path="tedxqu" element={<JoinUsPage />} />
-            <Route path="MainTheme" element={<MainTheme />} />
+            <Route path="tedxqu" element={<TEDxQU />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/TEDxQU.jsx
@@ -0,0 +1,13 @@
+import MainTheme from "../components/MainTheme";
+import SpeakersCarousel from "../components/SpeakersCarosalMobile";
+
+const TEDxQU = () => {
+  return (
+    <>
+      <MainTheme displayDescription={true} />
+      <SpeakersCarousel />
+    </>
+  );
+};
+
+export default TEDxQU;
